c477b37832387eb573d46554a797ef7cd7a15039
RangelGs-dev <rangelgs17@gmail.com>
2021-11-21 16:59:45 -0300

    refactoring css li

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const data = {
+    completed: []
+  }
 
-
+const removeSvg = '<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"width="24" height="24"viewBox="0 0 24 24"style=" fill:#ffff;"><path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"></path></svg>'
+/* Add item */
 document.querySelector('.addBtn').addEventListener('click', function() {
-    const value = this.value
+    const value = document.querySelector('.input-listAdd').value
     
     if(value) {
         addItem(value)
@@ -12,8 +16,7 @@ document.querySelector('.addBtn').addEventListener('click', function() {
 
 
 function handleKey(event) {
-    console.log(event)
-    const value = document.querySelector('.input-listAdd').value
+    const value = this.value
     if((event.key === 'Enter' || event.key === 'NumpadEnter') && value) {
         addItem(value)
     }
@@ -23,18 +26,42 @@ document.querySelector('.input-listAdd').addEventListener('keydown', handleKey)
 
 
 function addItem(value) {
-    const productList = document.querySelector('.list-items')
-    let li = document.createElement('li')
-    let span = document.createElement('span')
-    let img = document.createElement('img')
-    
+    addItemToDOM(value)
 
-    li.innerText = value
-    productList.appendChild(li)
-    
-    productList.appendChild(span)
-    span.appendChild(img)
-    img.classList.add('remove')
+    data.completed.push(value)
 
     document.querySelector('.input-listAdd').value = ''
+}
+
+
+function removeItem(event) {
+    let ItemTarget = event.target
+    let item = this.previousElementSibling
+    let parent = this.parentElement
+    parent.removeChild(this)
+    parent.removeChild(item)
+    
+}
+
+
+/* Adicionando itens à dom */
+function addItemToDOM(text) {
+    const productList = document.querySelector('.list-items')
+    let li = document.createElement('li')
+    let span = document.createElement('span')    
+
+    li.innerText = text
+    productList.appendChild(li)
+    li.classList.add('removeItem')
+    
+    productList.appendChild(span)
+    span.classList.add('removeSpan')
+    let button = document.createElement('button')
+    span.appendChild(button)
+    button.innerHTML = removeSvg
+    button.classList.add('remove')
+
+
+    /* adicionando evento de clique para remoção de item */
+    span.addEventListener('click', removeItem)
 }
